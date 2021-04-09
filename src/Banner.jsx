59ed05aa8ce848f39db07f8f129e8837a34a0594
mutua-mutinda <mutuamutinda@outlook.com>
2021-04-09 18:37:49 +0300

    implemented date picker component

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import { Button } from "@material-ui/core";
+import React, { useState } from "react";
 import "./Banner.css";
+import Search from "./Search";
 
 function Banner() {
-  return <div className="banner"></div>;
+  const [showSearch, setShowSearch] = useState(false);
+  return (
+    <div className="banner">
+      <div className="banner__search">
+        {showSearch && <Search />}
+        <Button
+          onClick={() => setShowSearch(!showSearch)}
+          className="banner__searchButton"
+          variant="outlined"
+        >
+          Search Dates
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
